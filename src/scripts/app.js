3b87c20a58f6b5a269f2582a0a3e10b730b63cf9
alexs222 <aleks.nikulin@gmail.com>
2018-03-25 21:55:29 +0200

    fixit indexHTML

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,13 @@
-const slider = require('./common/slider');
+import test from './common/_fullscreen_menu'
+import autorization from './common/_autorization';
 
-slider(); // инициализируем слайдер
+console.log('in index.js');
+test('Привет webpack');
+// autorization();
+var autorjs = autorization()
+console.log(autorjs)
+
+
+// const slider = require('./common/slider');
+
+// slider(); // инициализируем слайдерs
---
+++ src/scripts/common/_autorization.js
@@ -0,0 +1,5 @@
+export default function () {
+    const bottomAutorization = document.getElementById('autorization')
+    // var text = "autoriz"
+    return bottomAutorization
+}
