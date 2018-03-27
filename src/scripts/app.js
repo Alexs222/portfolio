933425b454124d764b2f084fb6640f4c3c3861c8
alexs222 <aleks.nikulin@gmail.com>
2018-03-27 20:05:59 +0200

    commit blog padge

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -24,16 +24,26 @@ console.log(panelOrder)
 console.log(buttonIndex)
 console.log(section)
 
+if (buttonAutorization !== null) {
+
+//     window.addEventListener('load', () => {
+//     section.classList.add('flipInX');
+// });
+
 
 buttonAutorization.addEventListener('click', (e) => {
     e.preventDefault();
-    buttonAutorization.classList.add('visuallyhidden')
-    // buttonAutorization.style.display = 'none';
+    buttonAutorization.style.display = 'none';
 
     panelInfo.classList.add('visuallyhidden')
     panelOrder.classList.remove('visuallyhidden')
-    buttonAutorization.classList.add('visuallyhidden')
-
-
 
 })
+
+buttonIndex.addEventListener('click', () => {
+    buttonAutorization.style.display = 'block';
+    panelOrder.classList.add('visuallyhidden')
+    panelInfo.classList.remove('visuallyhidden')
+})
+
+}
