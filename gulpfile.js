506aa32c34863989ed00ca5e65bf0ed9e8bd7d2e
alexs222 <aleks.nikulin@gmail.com>
2018-04-03 01:42:12 +0200

    fixit bugs

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('styles', function () {
             importer: moduleImporter()
         }))
         .pipe(autoprefixer({
-                browsers: ["last 2 versions"],
+                browsers: ["last 3 versions"],
                 cascade: false
             })
         )
